d6f4c28c683a6a7262d3a627cc1956fffb0a2fd3
Marvin Luchs <marvin.luchs@holisticon.de>
2025-07-28 22:43:27 +0200

    feat: add extractDocumentContents to DocumentAnalysisClient

--- src/application/server/DocumentAnalysisClient.ts
+++ src/application/server/DocumentAnalysisClient.ts
@@ -2,9 +2,16 @@ import type { FormRecognizerFeature } from '@azure/ai-form-recognizer';
 import { DocumentAnalysisClient as AzureDocumentAnalysisClient, AzureKeyCredential } from '@azure/ai-form-recognizer';
 import { getFileSize } from '../../common/server/file';
 import { resizeImage } from '../../common/server/image';
+import { sanitizeString } from '../../common/server/string';
 
 type Barcode = {
   ean13: string | null;
+};
+
+type DocumentContents = {
+  title: string | null;
+  pageNumber: number | null;
+  content: string;
 };
 
 export class DocumentAnalysisClient {
@@ -25,6 +32,19 @@ export class DocumentAnalysisClient {
 
     return {
       ean13: result.pages?.[0].barcodes?.find(({ kind }) => kind === 'EAN13')?.value ?? null,
+    };
+  }
+
+  async extractDocumentContents(apiClient: DocumentAnalysisClient, file: File): Promise<DocumentContents> {
+    const { content, paragraphs } = await this.analyzeDocument(file);
+
+    const title = paragraphs?.find(({ role }) => role === 'title')?.content ?? null;
+    const pageNumber = paragraphs?.filter(({ role, content }) => role === 'pageNumber' && /\d+/.test(content))[0]?.content ?? null;
+
+    return {
+      title: (title === null) ? null : sanitizeString(title),
+      pageNumber: (pageNumber === null) ? null : parseInt(pageNumber),
+      content,
     };
   }
 
