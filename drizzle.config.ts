dab858c064101329773bc27cbfe3fc325de9e91f
Marvin Luchs <marvin.luchs@holisticon.de>
2025-08-22 00:53:13 +0200

    fix: schema path in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './database',
-  schema: './src/databaseSchema.ts',
+  schema: './src/bootstrap/databaseSchema.ts',
   dialect: 'sqlite',
   dbCredentials: {
     url: process.env.DB_CONNECTION_STRING!,
